50b1f5a1394f0a5a170e084963b6b541daa04229
Stefanie Kobsar Rodríguez <Stephi@iMac.local>
2019-12-26 14:01:03 +0100

    Convert jupiternotebook to .py and add argparse

--- proj1/main.py
+++ proj1/main.py
@@ -1,13 +1,33 @@
-from operations import substract
-from operations import sumart
-from dimens.... import sumart
+from PIL import Image
+import rawpy
+import imageio
+import os
+import glob
+import argparse
 
 
-def choose_operation(operation):
+def transform_raw_jpg(raw_image, output_image):
+    with rawpy.imread(raw_image) as raw:
+        rgb = raw.postprocess()
+        imageio.imwrite(output_image, rgb)
+    return output_image
 
-    if operation == "sumar":
- 	sumar()
+############ARGPARSE########
+parser = argparse.ArgumentParser(description='Transform Raw to JPEG')
+parser.add_argument('input_folder', type=str, help='insert folder containing .ORF photos')
+parser.add_argument('--output_folder', default="Output_jpg" ,type=str, help='insert output folder for the JPEG photos')
+args = parser.parse_args()
+#############################
 
-    elif operation == "restar":
-	substractc()
+RAW_FOLDER = args.input_folder 
+folder = args.output_folder
 
+photos = glob.glob(RAW_FOLDER + "*.ORF")
+if not os.path.exists(folder):
+    os.mkdir(folder)
+
+for photo in photos:
+    photo_jpg = photo.strip(".ORF") + ".jpg"
+    photo_folder = os.path.join(folder, os.path.basename(photo_jpg))
+    print(photo_folder)
+    transform_raw_jpg(photo, photo_folder)
