f25973ca66858bde056f4165beb18f1361992583
Stefanie Kobsar Rodríguez <Stephi@iMac.local>
2020-01-09 19:52:06 +0100

    add commandline and api functionality

--- proj1/main.py
+++ proj1/main.py
@@ -12,22 +12,26 @@ def transform_raw_jpg(raw_image, output_image):
         imageio.imwrite(output_image, rgb)
     return output_image
 
-############ARGPARSE########
-parser = argparse.ArgumentParser(description='Transform Raw to JPEG')
-parser.add_argument('input_folder', type=str, help='insert folder containing .ORF photos')
-parser.add_argument('--output_folder', default="Output_jpg" ,type=str, help='insert output folder for the JPEG photos')
-args = parser.parse_args()
-#############################
 
-RAW_FOLDER = args.input_folder 
-folder = args.output_folder
+def transform(RAW_FOLDER, folder):
+    photos = glob.glob(RAW_FOLDER + "*.ORF")
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    
+    for photo in photos:
+        photo_jpg = photo.strip(".ORF") + ".jpg"
+        photo_folder = os.path.join(folder, os.path.basename(photo_jpg))
+        print(photo_folder)
+        transform_raw_jpg(photo, photo_folder)
 
-photos = glob.glob(RAW_FOLDER + "*.ORF")
-if not os.path.exists(folder):
-    os.mkdir(folder)
 
-for photo in photos:
-    photo_jpg = photo.strip(".ORF") + ".jpg"
-    photo_folder = os.path.join(folder, os.path.basename(photo_jpg))
-    print(photo_folder)
-    transform_raw_jpg(photo, photo_folder)
+
+if __name__ == "__main__":
+
+    #############ARGPARSE########
+    parser = argparse.ArgumentParser(description='Transform Raw to JPEG')
+    parser.add_argument('input_folder', type=str, help='insert folder containing .ORF photos')
+    parser.add_argument('--output_folder', default="Output_jpg" ,type=str, help='insert output folder for the JPEG photos')
+    args = parser.parse_args()
+    #############################
+    transform(args.input_folder, args.output_folder)
