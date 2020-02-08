1e4e68c93c2a36c81ece90f50e6270ebc0ebd7d2
Stefanie Kobsar Rodríguez <Stephi@iMac.local>
2020-02-08 13:56:29 +0100

    tests for png feature

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -1,6 +1,7 @@
 import pytest
 import os
-import image_formatter.main as mn
+import image_formatter.transform_algorithm.orf as orf
+import image_formatter.transform_algorithm.png as png
 
 
 absolute_path_every_machine = os.path.abspath(__file__)
@@ -10,7 +11,13 @@ directory_path = os.path.dirname(absolute_path_every_machine)
 def test_transform(image_orf=directory_path+"/data/P3030005.ORF", image_jpg="P3030005.jpg"):
     if os.path.exists(image_jpg):
         os.remove(image_jpg)
-    mn.transform_raw_jpg(image_orf, image_jpg)
+    orf.transform_raw_jpg(image_orf, image_jpg)
+    assert os.path.exists(image_jpg)
+
+def test_transform_png(image_png=directory_path+"/data/acorn.png", image_jpg="acorn.jpg"):
+    if os.path.exists(image_jpg):
+        os.remove(image_jpg)
+    png.transform_png_jpg(image_png, image_jpg)
     assert os.path.exists(image_jpg)
 
 
