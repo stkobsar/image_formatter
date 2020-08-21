082d682497357e3ada90d121b1797a67c77fa030
Stefanie Kobsar Rodríguez <stkobsar7@gmail.com>
2020-08-21 12:58:32 +0200

    update

--- image_formatter/transform_algorithm/raw.py
+++ image_formatter/transform_algorithm/raw.py
@@ -1,6 +1,6 @@
 import rawpy
 import imageio
-import argparse
+
 
 def transform_raw_jpg(raw_image, output):
     
@@ -16,16 +16,7 @@ def transform_raw_jpg(raw_image, output):
 
 
 
-if __name__ == "__main__":
 
-    #############ARGPARSE########
-    parser = argparse.ArgumentParser(description='Transform ORF to JPEG')
-    parser.add_argument('input_folder', type=str, help='insert folder containing .ORF photos')
-    parser.add_argument('format', type=str, help='insert photo format')
-    parser.add_argument('--output_folder', default="Output_jpg" ,type=str, help='insert output folder for the JPEG photos')
-    args = parser.parse_args()
-    #############################
-    transform(args.input_folder, args.output_folder, args.format)
 
 
 
