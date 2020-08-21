8242d320e7d3f4c12830afb75d9ac8eea12e3e2f
Stefanie Kobsar Rodríguez <stkobsar7@gmail.com>
2020-08-21 12:56:37 +0200

    change help in argparse

--- image_formatter/main.py
+++ image_formatter/main.py
@@ -1,54 +1,22 @@
-from PIL import Image
-import rawpy
-import imageio
-import os
-import glob
 import argparse
-import image_formatter.transform_algorithm.orf as orf
-import image_formatter.transform_algorithm.png as png
-
+import image_formatter.transform as tr
 
 SUPPORTED_FORMATS = ["ORF", "PNG", "RAW", "BMP"]
 
+def parse_args(parser):
+    parser.add_argument('-input', '--input_folder', type=str, help='insert folder with photos to convert')
+    parser.add_argument('-f', '--format', type=str, help='insert photo format')
+    parser.add_argument('-output', '--output_folder', default="output_jpg", type=str,
+                        help='insert output folder for the JPEG photos')
+    args = parser.parse_args()
 
-def transform(input_folder, output_folder, format):
-
-    if format.upper() in SUPPORTED_FORMATS:
-        input_path = input_folder + ".{}".format(format.upper())
-        photos = glob.glob(input_path)
-
-        if not photos:
-            input_path = input_folder + ".{}".format(format.lower())
-        photos = glob.glob(input_path)
-        if not os.path.exists(output_folder):
-            os.mkdir(output_folder)
-
-
-        for photo in photos:
-            print(photo)
-            photo = photo(photo, output_folder)
-            photo.transoform()
-            photo_jpg = photo.rsplit(".", 1)[0] + ".jpg"
-            photo_folder = os.path.join(output_folder, os.path.basename(photo_jpg))
-            print(photo_folder)
-            if format.upper() == "ORF":
-                orf.transform_raw_jpg(photo, photo_folder)
-            elif format.upper() == "PNG":
-                png.transform_png_jpg(photo, photo_folder)
-    else:
-        raise ValueError("Format not supported. Please choose between options {}".format(SUPPORTED_FORMATS))
 
 
 
 if __name__ == "__main__":
 
-    #############ARGPARSE########
     parser = argparse.ArgumentParser(description='Transform ORF to JPEG')
-    parser.add_argument('-input', '--input_folder', type=str, help='insert folder containing .ORF photos')
-    parser.add_argument('-f', '--format', type=str, help='insert photo format')
-    parser.add_argument('-output', '--output_folder', default="Output_jpg" ,type=str, help='insert output folder for the JPEG photos')
+    parse_args(parser)
     args = parser.parse_args()
-    #############################
 
-
-    transform(args.input_folder, args.output_folder, args.format)
+    tr.transform(args.input_folder, args.output_folder, args.format, SUPPORTED_FORMATS)
